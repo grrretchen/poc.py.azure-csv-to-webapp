724c7c3b377b1b1fe1ba02b6e980d47c3ab01b5e
Peter Keppel <pckeppel@gmail.com>
2023-03-09 15:03:07 -0600

    import numpy and generate seed

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import random
+import numpy as np
 
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
+
+np.random.default_rng(2021).random(4)[1]
 
 @csrf_exempt
 def hello(request):
@@ -19,7 +22,8 @@ def hello(request):
             print("Request for hello page received with name=%s" % name)
             context = {
                 'name': name,
-                'random' : random.randint(1,100)
+                'random' : random.randint(1,100),
+                'seed' : np.random.default_rng(2021).random(4)[1]
                 }
             return render(request, 'hello_azure/hello.html', context)
     else:
